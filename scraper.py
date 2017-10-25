f2fd3ed0f8e2cdbd0dd9f7b38591bf1d76684f82
thomas <thomasdebeus@users.noreply.github.com>
2017-10-25 15:35:42 +0200

    ISO wikipedia scraper
    https://classic.scraperwiki.com/editor/raw/iso_3166-1

--- scraper.py
+++ scraper.py
@@ -1,22 +1,41 @@
-import urllib2
-from xml.dom.minidom import parseString
+import scraperwiki
+import lxml.html
+import urllib
+import datetime
+import json
 
-def get_google_new_results( term, count ):
-    results = []
-    obj = parseString( urllib2.urlopen('http://news.google.com/news?q=%s&output=rss' % term).read() )
+from unidecode import unidecode
 
-    elements = obj.getElementsByTagName('title')[2:] # To get rid of unwanted title elements in XML doc    
-    links = obj.getElementsByTagName('link')[2:]
-    print links
-    for element in elements[:count]:
-        headline =  element.childNodes[0].data
-        for link in links:
-            url = link.childNodes[0].data.split('=')[-1]
-        newssearch = headline + ' -> ' + url
-        results.append( newssearch )
+def get_html(title):
+    raw_json = scraperwiki.scrape("http://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + title)
+    html = json.loads(raw_json)['parse']['text']['*']
+    return html
 
-    return results
+page_title = "ISO_3166-1"
 
-items = get_google_new_results( 'Wired', 10 )
-for i,e in enumerate(items):
-    print '%d: %s' % (i+1,e,)
+html = get_html("https://classic.scraperwiki.com/editor/raw/iso_3166-1")
+doc = lxml.html.fromstring(html)
+
+for count, tr in enumerate(doc.cssselect('tr')):
+    row = [(td.text_content()) for td in tr.cssselect('td')]
+    if len(row)==5:
+        for ahref in tr.cssselect('a'):
+            detailink = ahref.attrib['href']
+            if detailink.find(':',0,len(detailink)) != -1:
+                detailink = detailink[6:]
+                print detailink
+        now = datetime.datetime.now()
+        data ={"tmsp_scraped":str(now), "eng_short_name":row[0], "alpha_2_code":row[1], "alpha_3_code":row[2], "numeric_code":row[3], "iso_31662_code":detailink}
+        scraperwiki.sqlite.save(unique_keys=["eng_short_name"], data=data, table_name="s_iso31661")
+        
+        html = get_html(detailink)
+        doc = lxml.html.fromstring(html)
+
+        for count, tr in enumerate(doc.cssselect('tr')):
+            row = [td.text_content() for td in tr.cssselect('td')]
+            row2 = [td.text_content() for td in tr.cssselect('td')]
+            if len(row)>0:
+                if row[0][:2] == detailink[11:]:
+                    now = datetime.datetime.now()
+                    data = {"tmsp_scraped":str(now), "iso_31662_code":detailink, "region_code":row[0], "region_desc":row[1], "region_desc_utf8":row2[1]}
+                    scraperwiki.sqlite.save(unique_keys=["iso_31662_code","region_code"], data=data, table_name="s_iso31662_region")
